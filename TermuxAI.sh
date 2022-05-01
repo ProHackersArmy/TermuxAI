01ae53c4450bcda8c5bd2a07b2dd67d7847c12a1
ParzivalKali <82817793+ParzivalHack@users.noreply.github.com>
2022-05-01 15:44:25 +0200

    Update TermuxAI.sh

--- TermuxAI.sh
+++ TermuxAI.sh
@@ -1,59 +1,74 @@
-#-----------------
-# scripting start
+red="\e[34m"
+rset="\e[34m"
+grn="\e[34m"
+ylo="\e[34m"
+blue="\e[34m"
+cyan="\e[34m"
+pink="\e[34m"
 banner(){
 vid
-echo -e "\e[40;38;5;82m     
-_  _
-|  \/  | _ _ __  _   _
-| |\/| |/ _ \ '_ \| | | |
-| |  | |  _/ | | | || |
-||  ||\__|| ||\,|
+echo -e "\e[34m     
+..:!JYYYJ?^^?JYYYJ!:.                                        
+                                     .?55G577?JJYBGYJJ?7?PP557.                                     
+   ..                          ..:..7PG7!!57JY7??YJ??75?7Y!!?G5!                         ..:.       
+....                         .:::..5P7?JY77YYY?J?!!J??YYY!7JJ77PY                      ..:::.       
+.                         ..:::.  7#J?!!7Y?!7JYYJJJJYYJ7~?J7~!?J#!                   .:::..         
+                          .::.   JB?7Y?JJ75?YJJ?7777?JJY?Y7J?JY7JB7                  .:..           
+                                @5!7Y?57Y?5??JJJ!YJ???Y?J75?Y7!G@^                                
+                             ^7JG@@#GJ7YJ?7?J?7Y!!57?J?7?JJ7YG#@@PJ7^                             
+                            &@@@@@@@@&#BBGG5YP5?GPJPPJ5GGBB#&@@@@@@@@&                            
+                            !@@@@@@@@@@@@@@@@@@&@@@@@@@@@@@@@@@@@@@@@@@~                            
+              ::.            Y@@@@@@@@@@@@@&##&@@@@@@@@@@@@@@@@@@@@@@@Y .::.                        
+              ...             Y@@@@&5J777?Y5G#@@@@@@@@&BPY?777JP&@@@@Y   ...                        
+                               7#@&~         :JP&&GJ:         ^&@B7                               
+                                ?@@?     ^!~.    ::    .~7^     ?@@!                                
+                                ?@@@P!.  :77:          .77:  .!P@@@!                                
+   .                            7@@@@@#5?^.            . .^75#@@@@@!                                
+..:::.                          7#&@@@@@@@&Y. JP?  ?PJ .Y&@@@@&&@@@!                      ...       
+::..                         .. 7@@@@@@@@@?.  !B#PP&B7  .J&&&&@@@@@!                   ....         
+..                         ...  7@@@@@@@@7      YY   .:.?&@@@@@@@~                  ..            
+                           .    :&@@@@@@P               .. P@@@@@@#.                                
+                                 7@@@@@@?                  ?@@@@@&!                                 
+                                  !#@@@@Y                  Y@@@@B~                                  
+                                   :Y#@@&~ .^!7JJYYYJ7!^. ~&@@#J.                                   
+                                     :7P##Y!~^^:::::^^~Y##57.                                     
+               .                        .J5?.       :7Y?~.             ..                         
+                                           .!?J??????J?!.                                           
+                                               .::::.
                                                MonkeAI
                                        Developed by ParzivalHack
 "
-
+echo ""
+echo
+echo -e "$red Hello Sir, how can i help you today? $rset"
 echo " "
-menu(){
-echo -e "$grn             ＞＞＞＞>>>＞$ylo [Options]$grn ＜<<<＜＜＜＜$rset"
-echo " "
-echo " "
-echo -e "$red                        ➡$cyan [\e[91m1\e[91m] Chat with MonkeAI"
-echo -e "$red                        ➡$cyan [\e[91m2\e[91m] About me"
-echo -e "$red                        ➡$cyan [\e[91m3\e[91m] Update script "
-echo -e "$red                        ➡$cyan [\e[91m4\e[91m] Check out my VPN"
-echo -e "$red                        ➡$cyan [\e[91m5\e[91m] Report issues"
-echo -e "$red                        ➡$cyan [\e[91m6\e[91m] Exit"
-echo " "
-echo " "
-echo -e "$grn               >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
-echo " "
-echo -ne "\e[91m#SELECT OPTION\e[91m: "
-menu(){
+echo -ne "\e[34m#Say something to TermuxAI\e[34m: "
 read optnz
-if [ $optnz = "1" ];
+if [ $optnz = "hi" ];
 then
-assistant
-elif [ $optnz = "2" ];
+echo -e "$red Hello Sir, how can i help you today? $rset"
+elif [ $optnz = "help" ];
+then
+echo -e "$red Commands list: hi - credits - update - vpn - report - exit $rset"
+elif [ $optnz = "credits" ];
 then
 am start -a android.intent.action.VIEW -d https://msha.ke/tommasobona/ > /dev/null 2>&1
 banner
 menu
-elif [ $optnz = "3" ];
-update
-banner
-menu
-sed -i 's/\r//' monkeAI.sh
-elif [$optnz = "4"];
+elif [ $optnz = "update" ];
+then
+upd
+elif [ $optnz = "vpn" ];
 then
 am start -a android.intent.action.VIEW -d https://www.whispervpn.company.site/ > /dev/null 2>&1
 banner
 menu
-elif [ $optnz = "5" ];
+elif [ $optnz = "report" ];
 then
-am start -a android.intent.action.VIEW -d https://github.com/ParzivalHack/MonkeAI/issues > /dev/null 2>&1
+am start -a android.intent.action.VIEW -d https://github.com/ParzivalHack/TermuxAI/issues > /dev/null 2>&1
 banner
 menu
-elif [ $optnz = "6" ];
+elif [ $optnz = "exit" ];
 then
 exit 1
 else
@@ -61,52 +76,7 @@ echo "wrong"
 exit
 fi
 }
-echo " "
-echo -e "\e[40;38;5;82m Hello sir, how can i help you? "
-echo
-echo -ne "\e[91m#Type here: "
-read optnz
-if [ $optnz = " hi " ];
-then
-echo "\e[40;38;5;82m Hello sir "
-banner
-assistant
-elif [ $optnz = " open youtube " ];
-then
-am start -a android.intent.action.VIEW -d https://www.youtube.com/ > /dev/null 2>&1
-banner
-assistant
-elif [ $optnz = " help " ];
-then
-echo "\e[40;38;5;82m WARNING: This is the Beta version of the MonkeAI Terminal Assistant. Feel free to report any bug you find. © 2022 Tommaso Bona. All rights reserved. RULES: Don't use caps and write one command per time. Type "help" to get this message. COMMANDS LIST: open youtube - open twitch - open google - open discord - how are you? - hi - help. "
-banner
-assistant
-elif [ $optnz = " open google " ];
-then
-am start -a android.intent.action.VIEW -d https://www.google.com/ > /dev/null 2>&1
-banner
-assistant
-elif [ $optnz = " open twitch " ];
-then
-am start -a android.intent.action.VIEW -d https://www.twitch.tv/ > /dev/null 2>&1
-banner
-assistant
-elif [ $optnz = " open discord " ];
-then
-am start -a android.intent.action.VIEW -d https://www.discord.com/ > /dev/null 2>&1
-banner
-assistant
-if [ $optnz = " how are you " ];
-then
-echo "\e[40;38;5;82m I'm great Sir, thanks for asking :D "
-banner
-assistant
-else
-banner
-assistant
-fi
-}
-update(){
+upd(){
 if [ -d "$HOME/TermuxAI" ];
 then
 cd $HOME
@@ -121,25 +91,58 @@ exit 1
 fi
 cd $HOME
 sleep 1
-echo -e "         \e[91mUPDATE IS GOING ON, PLEASE BE PATIENT...!\e[0m"
+echo -e "         \e[34mUPDATE IS GOING ON, PLEASE BE PATIENT...!\e[0m"
 echo
-printf "                     \e[96m["
+printf "                     \e[34m["
 # While process is running...
 while git clone https://github.com/ParzivalHack/TermuxAI 2> /dev/null; do 
-    printf  "\e[91m▓▓▓▓▓▓▓▓▓▓▓▓▓\e[0m"
+    printf  "\e[34m▓▓▓▓▓▓▓▓▓▓▓▓▓\e[0m"
     sleep 1
 done
-printf "\e[91m]\e[0m"
+printf "\e[34m]\e[0m"
 echo
 echo
 echo
-printf "\e[91m           UPDATED SUCCESSFULLY TO THE LATEST VERSION!\e[0m"
+printf "\e[34m           UPDATED SUCCESSFULLY TO THE LATEST VERSION!\e[0m"
 sleep 2.0
 clear
 cd $HOME
-cd TermuxAI
+cd doomshell
 bash TermuxAI.sh
 }
+vid(){
+FILE=$HOME/TermuxAI
+if [ -f "$FILE" ]; then
+pop
+else
+echo
+fi
+}
+pop(){
+clear
+read p
+if [ "$p" = "y" ];
+then
+am start -a android.intent.action.VIEW -d ERROR000 2>/dev/null
+clear
+baner1
+menu
+elif [ "$p" = "t" ];
+then
+clear
+baner1
+menu
+elif [ "$p" = "z" ];
+then
+cd $HOM/TermuxAI
+rm noob.noob
+banner
+menu
+else
+banner
+menu
+exit
+fi
 }
 banner
 menu
